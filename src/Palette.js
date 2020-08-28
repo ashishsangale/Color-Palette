27cd66bcac0b0830b53b171c9842a2c776bc8b27
Ashish Sangale <aashishsangale07@gmail.com>
2020-08-28 20:26:09 +0530

    Palette component

---
+++ src/Palette.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Palette extends Component {
+    render() {
+        return (
+            <div className='Palette'>
+                <h1>Palette</h1>
+                <div className='Palette-colors'>
+                    
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Palette;
