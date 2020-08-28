cf3d42365333b087d9f901bfeca334a4839ffaae
Ashish Sangale <aashishsangale07@gmail.com>
2020-08-28 21:22:15 +0530

    adding colorbox component to palette

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import seedColors from './seedColors';
 function App() {
   return (
     <div className="App">
-      <Palette {...seedColors}/>
+      <Palette {...seedColors[6]}/>
     </div>
   );
 }
