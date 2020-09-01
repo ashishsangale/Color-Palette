16412dda15baa3527b27620b6bf88c4fe5e0996a
Ashish Sangale <aashishsangale07@gmail.com>
2020-09-01 16:53:01 +0530

    Colorhelper.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './App.css';
 import Palette from './Palette'
 import seedColors from './seedColors';
+import {generatePalette} from './Colorhelper'
 
 function App() {
+  console.log(generatePalette(seedColors[6]))
   return (
     <div className="App">
       <Palette {...seedColors[6]}/>
