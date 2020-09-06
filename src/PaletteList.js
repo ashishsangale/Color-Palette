c4c3fc5b4e46f05c1b9d7cb3de7b4a563299486e
Ashish Sangale <aashishsangale07@gmail.com>
2020-09-06 14:05:10 +0530

    adding minipalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+//import {Link} from 'react-router-dom';
+import Minipalette from './Minipalette';
 
 class PaletteList extends Component {
 
     render() {
         const {palettes} = this.props
         const paletteList = palettes.map(palette => (
-            <p>
-                <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-            </p>
+            <Minipalette {...palette}/>
         ))
 
         return (
             <div>
+                
                 <h1>COLOR PALETTE</h1>
                 {paletteList}
             </div>
