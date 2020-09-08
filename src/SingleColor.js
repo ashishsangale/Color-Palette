2e839ff62946012f24197e42a94518333b40eb1c
Ashish Sangale <aashishsangale07@gmail.com>
2020-09-08 23:55:54 +0530

    SingleColor palette

---
+++ src/SingleColor.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SingleColor extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Helloo </h1>
+            </div>
+        )
+    }
+}
+
+export default SingleColor;
