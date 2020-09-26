4e0a895609a5c7fcfda5f4a33b8ac6490775edd1
Ashish Sangale <aashishsangale07@gmail.com>
2020-09-26 11:28:01 +0530

    adding color-picker

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -10,8 +10,10 @@ import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Button from '@material-ui/core/Button'
+import {ChromePicker} from 'react-color';
 
-const drawerWidth = 240;
+const drawerWidth = 400;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,7 +27,7 @@ const useStyles = makeStyles((theme) => ({
   },
   appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
+    marginLeft: drawerWidth, 
     transition: theme.transitions.create(['margin', 'width'], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
@@ -121,7 +123,16 @@ export default function NewPaletteForm() {
             <ChevronLeftIcon />
           </IconButton>
         </div>
+        <Typography variant="h6" noWrap>
+            Make your own Palette
+        </Typography>
+        <div>
+          <Button variant='contained' color='secondary'>Clear Palette</Button>
+          <Button variant='contained' color='primary'>Random Color</Button>
+        </div>
         <Divider/>
+        <ChromePicker color='red' onChangeComplete={(newColor) => console.log(newColor)}/>
+        <Button variant='contained' color='primary'>Add Color</Button>
       </Drawer>
       <main
         className={clsx(classes.content, {
@@ -129,7 +140,6 @@ export default function NewPaletteForm() {
         })}
       >
         <div className={classes.drawerHeader} />
-        
       </main>
     </div>
   );
