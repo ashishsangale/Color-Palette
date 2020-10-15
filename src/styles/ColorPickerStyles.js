c9a2a7f67e56be6b4185e09fd7716b7262b65dce
Ashish Sangale <aashishsangale07@gmail.com>
2020-10-15 17:25:08 +0530

    Exporting styles to seperate files

---
+++ src/styles/ColorPickerStyles.js
@@ -0,0 +1,18 @@
+const styles ={
+    picker:{
+        width: '100% !important',          //over-rule parent styles
+        marginTop: '2rem'
+    },
+    addColor: {
+        width: '100%',
+        padding: '0.7rem',
+        marginTop: '1rem',
+        fontSize: '2rem'
+    },
+    input:{
+        width: '100%',
+        height: '70px'
+    }
+}
+
+export default styles;
