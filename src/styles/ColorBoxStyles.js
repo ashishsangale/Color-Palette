0e7df849620883e150341521d06e4e314c146010
Ashish Sangale <aashishsangale07@gmail.com>
2020-10-19 20:52:36 +0530

    Responsive sizes for color box

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -1,4 +1,5 @@
-import chroma from 'chroma-js'
+import chroma from 'chroma-js';
+import sizes from './Sizes';
 export default {
     colorBox:{
         width: '20%',
@@ -10,6 +11,18 @@ export default {
         marginBottom: '-4.5px',
         '&:hover button':{
             opacity: 1
+        },
+        [sizes.down('lg')]:{
+            width: '25%',
+            height: props => (props.fullPalette)? '20%': '50%'
+        },
+        [sizes.down('md')]:{
+            width: '50%',
+            height: props => (props.fullPalette)? '10%': '50%'
+        },
+        [sizes.down('xs')]:{
+            width: '100%',
+            height: props => (props.fullPalette)? '5%': '50%'
         }
     },
     copyText: {
